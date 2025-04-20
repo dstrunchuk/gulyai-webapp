c280f6404851cd50a86dc8cf3ab1a67cf73c875d
sell <sell@MacBook-Pro-sell.local>
2025-04-20 15:34:07 +0300

    Обновление

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -94,8 +94,8 @@ const People = () => {
                 <p className="text-lg font-bold text-center mb-1">{person.name}</p>
                 <p className="text-center text-sm text-gray-400 mb-2">{person.age} лет</p>
                 <p className="text-center text-sm text-gray-400">{person.address}</p>
-                <p><span className="text-zinc-400">Цель:</span> {user.activity}</p>
-                <p><span className="text-zinc-400">Настроение:</span> {user.vibe}</p>
+                <p><span className="text-zinc-400">Цель:</span> {person.activity}</p>
+                <p><span className="text-zinc-400">Настроение:</span> {person.vibe}</p>
                 {dist !== null && (
                   <p className="text-center text-xs text-gray-500 mt-1">{dist} метров от тебя</p>
                 )}

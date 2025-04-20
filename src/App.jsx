9407263daa02a72b221afe08627f99227bfebc7b
sell <sell@MacBook-Pro-sell.local>
2025-04-20 20:22:14 +0300

    Обновление

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Form from "./pages/Form";
 import Profile from "./pages/Profile";
 import People from "./pages/People";
 import ViewProfile from "./pages/ViewProfile";
 
+
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Form />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/people" element={<People />} />
-        <Route path="/view-profile/:chat_id" element={<ViewProfile />} />
-      </Routes>
-    </Router>
+  <Routes>
+    <Route path="/" element={<Form />} />
+    <Route path="/profile" element={<Profile />} />
+    <Route path="/people" element={<People />} />
+    <Route path="/view-profile/:chat_id" element={<ViewProfile />} />
+  </Routes>
+</Router>
   );
 }
 

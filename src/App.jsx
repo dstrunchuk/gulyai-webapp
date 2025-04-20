8d172615e9e8101e9b32a2126363f8a8a5e0df4e
sell <sell@MacBook-Pro-sell.local>
2025-04-20 15:22:03 +0300

    Обновление

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Form from "./pages/Form";
 import Profile from "./pages/Profile";
+import People from "./pages/People"; // путь укажи правильный, если у тебя файл в другом месте
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/people" element={<People />} />
       </Routes>
     </Router>
   );
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+
 const Profile = () => {
   const [user, setUser] = useState(null);
   const [statusDuration, setStatusDuration] = useState(1);

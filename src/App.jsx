30cc034a82f5434cc4d41508d962956becf93b6f
sell <sell@MacBook-Pro-sell.local>
2025-04-20 20:04:10 +0300

    Обновление

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Form from "./pages/Form";
 import Profile from "./pages/Profile";
 import People from "./pages/People"; // путь укажи правильный, если у тебя файл в другом месте
